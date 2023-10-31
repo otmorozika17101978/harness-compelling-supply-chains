7ad844e4b965deb148de8f8467fdbde4448a16a4
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-10-31 18:39:30 +0300

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,33 @@
+        console.log("Random Number: " + item);
+}
+    return data;
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
+function main() {
+main();
+    data.forEach(item => {
+    const data = [];
+    });
+
+}
+    }
+
+    for (let i = 0; i < 10; i++) {
+
+function generateRandomData() {    return data;
+    const data = generateRandomData();
+        console.log("Random Number: " + item);
+
+    data.forEach(item => {
+}
+
+
+    const data = [];
+    });
+    for (let i = 0; i < 10; i++) {
+    }
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+}
+function main() {
+function generateRandomData() {
